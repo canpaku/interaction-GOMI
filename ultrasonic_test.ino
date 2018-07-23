f2ec2f09f7dd8274429d5ccaf824d97e787b0ea6
lisaco-ono <lisaco.ono.ii@gmail.com>
2018-07-23 16:03:14 +0900

    パタパタがうまくいったところ

--- ultrasonic_test.ino
+++ ultrasonic_test.ino
@@ -34,11 +34,12 @@ int motorAngle_label;
 int motorAngle_bottle;
 
 //speaker(buzzer)
-int speakerPin = 11; 
+int speakerPin = 11;
 
 //infrared
 int infraredPin = 7;
 int judge_infrared;
+int infra_flag = 0;
 
 //which_term : cap/label/bottle
 int which_term;
@@ -60,7 +61,7 @@ void setup() {
   motorAngle_cap = CLOSE;
   motorAngle_label = CLOSE;
   motorAngle_bottle = CLOSE;
-  
+
   Motor_cap.write(motorAngle_cap);
   Motor_label.write(motorAngle_label);
   Motor_bottle.write(motorAngle_bottle);
@@ -69,12 +70,12 @@ void setup() {
   n_label = 0;
   n_bottle = 0;
 
-  distance_cap = 0;
-  just_throwed_cap = 0;
-  distance_label = 0;
-  just_throwed_label = 0;
-  distance_bottle = 0;
-  just_throwed_bottle = 0;
+  distance_cap = 10000;
+  just_throwed_cap = 100000;
+  distance_label = 10000;
+  just_throwed_label = 10000;
+  distance_bottle = 10000;
+  just_throwed_bottle = 10000;
 
   judge_infrared = LOW;
 
@@ -83,223 +84,206 @@ void setup() {
   countNum = 0;
 }
 
-void thankYou(Servo mc, Servo ml, Servo mb){
-  mc.write(CLOSE);
-  ml.write(CLOSE);
-  mb.write(CLOSE);
-  //ぱぱぱ、
-  delay(500);
-  mc.write(YEAH);
-  delay(500);
-  ml.write(YEAH);
-  delay(500);
-  mb.write(YEAH);
-  delay(1000);
-  //ぱぱぱ、
-  mb.write(CLOSE);
-  delay(500);
-  ml.write(CLOSE);
-  delay(500);
-  mc.write(CLOSE);
-  delay(1000);
-  //ぱ、ぱ、ぱ！
-  mc.write(YEAH);
-  ml.write(LITTLE_OPEN);
-  mb.write(YEAH);
-  delay(1000);
-  mc.write(LITTLE_OPEN);
-  ml.write(YEAH);
-  mb.write(LITTLE_OPEN);
-  delay(1000);
-  mc.write(YEAH);
-  ml.write(YEAH);
-  mb.write(YEAH);
-  delay(1000);
-}
+//void thankYou(Servo mc, Servo ml, Servo mb){
+//  mc.write(CLOSE);
+//  ml.write(CLOSE);
+//  mb.write(CLOSE);
+//  //ぱぱぱ、
+//  delay(500);
+//  mc.write(YEAH);
+//  delay(500);
+//  ml.write(YEAH);
+//  delay(500);
+//  mb.write(YEAH);
+//  delay(1000);
+//  //ぱぱぱ、
+//  mb.write(CLOSE);
+//  delay(500);
+//  ml.write(CLOSE);
+//  delay(500);
+//  mc.write(CLOSE);
+//  delay(1000);
+//  //ぱ、ぱ、ぱ！
+//  mc.write(YEAH);
+//  ml.write(LITTLE_OPEN);
+//  mb.write(YEAH);
+//  delay(1000);
+//  mc.write(LITTLE_OPEN);
+//  ml.write(YEAH);
+//  mb.write(LITTLE_OPEN);
+//  delay(1000);
+//  mc.write(YEAH);
+//  ml.write(YEAH);
+//  mb.write(YEAH);
+//  delay(1000);
+//}
 
 /////////----------  loop  ---------------/////////
 
 void loop() {
-//  timer = millis();
-  
+  //  timer = millis();
+
   distance_cap = Us_judge_hand_cap.MeasureInCentimeters();
-  just_throwed_cap = Us_throwed_cap.MeasureInCentimeters();
-  
-  distance_label = Us_judge_hand_label.MeasureInCentimeters();
-  just_throwed_label = Us_throwed_label.MeasureInCentimeters();
-  
-  distance_bottle = Us_judge_hand_bottle.MeasureInCentimeters();
-//  just_throwed_bottle = Us_throwed_bottle.MeasureInCentimeters();
+  //  just_throwed_cap = Us_throwed_cap.MeasureInCentimeters();
+
+  //  distance_label = Us_judge_hand_label.MeasureInCentimeters();
+  //  just_throwed_label = Us_throwed_label.MeasureInCentimeters();
+
+  //  distance_bottle = Us_judge_hand_bottle.MeasureInCentimeters();
+  //  just_throwed_bottle = Us_throwed_bottle.MeasureInCentimeters();
 
   judge_infrared = digitalRead(infraredPin);
 
 
   ////- - - - - - - -infrared sensor- - - - - - //////
-  
-  if (judge_infrared == HIGH){
+
+  if (judge_infrared == HIGH && infra_flag == false) {
     //パ
+    infra_flag = true;
     motorAngle_cap = LITTLE_OPEN;
     motorAngle_label = LITTLE_OPEN;
     motorAngle_bottle = LITTLE_OPEN;
-    
-    n_cap = countNum;
-    n_label = countNum;
-    n_bottle = countNum;
-  }
-  
-  //キャップ
-  if (motorAngle_cap == LITTLE_OPEN){//パタパタの「パ」の角度で
-    if((countNum - n_cap) > 100){//かつ「パ」になったタイミングから100カウント済んでいたら
-      motorAngle_cap = CLOSE;
-      n_cap = countNum;
-    }
-  } else if (motorAngle_cap == CLOSE){//パタパタの「タ」の角度で
-    if((countNum - n_cap) > 100){//100カウント済んでいたら
-      motorAngle_cap = LITTLE_OPEN;
-      n_cap = countNum;
-    }
-  }
-  //ラベル
-  if (motorAngle_label == LITTLE_OPEN){//パタパタの「パ」の角度で
-    if((countNum - n_label) > 100){//かつ「パ」になったタイミングから100カウント済んでいたら
-      motorAngle_label = CLOSE;
-      n_label = 0;
-    }
-  } else if (motorAngle_label == CLOSE){//パタパタの「タ」の角度で
-    if((countNum - n_label) > 100){//100カウント済んでいたら
-      motorAngle_label = LITTLE_OPEN;
-      n_label = countNum;
-    }
-  }
-  //ボトル
-  if (motorAngle_bottle == LITTLE_OPEN){//パタパタの「パ」の角度で
-    if((countNum - n_bottle) > 100){//かつ「パ」になったタイミングから100カウント済んでいたら
-      motorAngle_bottle = CLOSE;
-      n_bottle = 0;
-    }
-  } else if (motorAngle_bottle == CLOSE){//パタパタの「タ」の角度で
-    if((countNum - n_bottle) > 100){//100カウント済んでいたら
-      motorAngle_bottle = LITTLE_OPEN;
-      n_bottle = countNum;
-    }
-  }
 
-  ////- - - - - - - -ultrasonic sensor- - - - - - //////
-  ////-  .  -  .  -手が近づいたらver.  .  -  .  -  .//////
-
-  //キャップ
-  if(distance_cap < 8){
-    if(which_term == CAP){
-      motorAngle_cap = FULL_OPEN;
-    } else {//ぴしゃ
-      Motor_cap.write(175);
-      delay(300);
-      Motor_cap.write(CLOSE);
-      delay(300);
-      //ここでパタパタの「パ」の状態に戻しておく
-      motorAngle_cap = LITTLE_OPEN;
-      n_cap = countNum;
-    }
-  }
-  //ラベル
-  if(distance_label < 8){
-    if(which_term == LABEL){
-      motorAngle_label = FULL_OPEN;
-    } else {
-      //ぴしゃ
-      Motor_label.write(175);
-      delay(300);
-      Motor_label.write(CLOSE);
-      delay(300);
-      motorAngle_label = LITTLE_OPEN;
-      n_label = countNum;
-    }
-  }
-  //ボトル
-  if(distance_bottle < 8){
-    if(which_term == BOTTLE){
-      motorAngle_bottle = FULL_OPEN;
-    } else {
-      //ぴしゃ
-      Motor_bottle.write(175);
-      delay(300);
-      Motor_bottle.write(CLOSE);
-      delay(300);
-      motorAngle_bottle = LITTLE_OPEN;
-      n_bottle = countNum;
-    }
-  }
-
-  ////- - - - - - - -ultrasonic sensor- - - - - - //////
-  ////-  .  -  .  -ゴミが捨てられたらver.  .  -  .  -  .//////
-
-  if((just_throwed_cap < 5) && (which_term == CAP)){
-    which_term = LABEL;
-  }
-  if((just_throwed_label < 5) && (which_term == LABEL)){
-    which_term = BOTTLE;
-  }
-  if((just_throwed_bottle < 5) && (which_term == BOTTLE)){
-    //全員でよろこぶ！
-//    thankYou(Motor_cap, Motor_label, Motor_bottle);
-
-    which_term = CAP;
-
-    //パタパタの「ぱ」状態に戻す
-    motorAngle_cap = LITTLE_OPEN;
-    motorAngle_label = LITTLE_OPEN;
-    motorAngle_bottle = LITTLE_OPEN;
-    
     n_cap = countNum;
     n_label = countNum;
     n_bottle = countNum;
   }
 
-  //毎ループここでモーターの角度をmotorAngleにする
-  Motor_cap.write(motorAngle_cap);
-  Motor_label.write(motorAngle_label);
-  Motor_bottle.write(motorAngle_bottle);
-  
+  if (infra_flag == true) {
+    //キャップ
+    if (motorAngle_cap == LITTLE_OPEN) { //パタパタの「パ」の角度で
+      if ((countNum - n_cap) == 50) { //かつ「パ」になったタイミングから100カウント済んでいたら
+        motorAngle_cap = CLOSE;
+        n_cap = countNum;
+      }
+    } else if (motorAngle_cap == CLOSE) { //パタパタの「タ」の角度で
+      if ((countNum - n_cap) == 50) { //100カウント済んでいたら
+        motorAngle_cap = LITTLE_OPEN;
+        n_cap = countNum;
+      }
+    }
+    //ラベル
+    if (motorAngle_label == LITTLE_OPEN) { //パタパタの「パ」の角度で
+      if ((countNum - n_label) == 50) { //かつ「パ」になったタイミングから100カウント済んでいたら
+        motorAngle_label = CLOSE;
+        n_label = countNum;
+      }
+    } else if (motorAngle_label == CLOSE) { //パタパタの「タ」の角度で
+      if ((countNum - n_label) == 50) { //100カウント済んでいたら
+        motorAngle_label = LITTLE_OPEN;
+        n_label = countNum;
+      }
+    }
+    //ボトル
+    if (motorAngle_bottle == LITTLE_OPEN) { //パタパタの「パ」の角度で
+      if ((countNum - n_bottle) == 50) { //かつ「パ」になったタイミングから100カウント済んでいたら
+        motorAngle_bottle = CLOSE;
+        n_bottle = countNum;
+      }
+    } else if (motorAngle_bottle == CLOSE) { //パタパタの「タ」の角度で
+      if ((countNum - n_bottle) == 50) { //100カウント済んでいたら
+        motorAngle_bottle = LITTLE_OPEN;
+        n_bottle = countNum;
+      }
+    }
+
+    ////- - - - - - - -ultrasonic sensor- - - - - - //////
+    ////-  .  -  .  -手が近づいたらver.  .  -  .  -  .//////
+
+    //キャップ
+    if (distance_cap < 8) {
+      if (which_term == CAP) {
+        motorAngle_cap = FULL_OPEN;
+      } else {//ぴしゃ
+        //      Motor_cap.write(175);
+        //      delay(300);
+        Motor_cap.write(CLOSE);
+        delay(300);
+        //ここでパタパタの「パ」の状態に戻しておく
+        motorAngle_cap = LITTLE_OPEN;
+        n_cap = countNum;
+      }
+    }
+    //ラベル
+    if (distance_label < 8) {
+      if (which_term == LABEL) {
+        motorAngle_label = FULL_OPEN;
+      } else {
+        //ぴしゃ
+        //      Motor_label.write(175);
+        //      delay(300);
+        Motor_label.write(CLOSE);
+        delay(300);
+        motorAngle_label = LITTLE_OPEN;
+        n_label = countNum;
+      }
+    }
+    //ボトル
+    if (distance_bottle < 8) {
+      if (which_term == BOTTLE) {
+        motorAngle_bottle = FULL_OPEN;
+      } else {
+        //ぴしゃ
+        //      Motor_bottle.write(175);
+        //      delay(300);
+        Motor_bottle.write(CLOSE);
+        delay(300);
+        motorAngle_bottle = LITTLE_OPEN;
+        n_bottle = countNum;
+      }
+    }
+
+    ////- - - - - - - -ultrasonic sensor- - - - - - //////
+    ////-  .  -  .  -ゴミが捨てられたらver.  .  -  .  -  .//////
+
+    if ((just_throwed_cap < 5) && (which_term == CAP)) {
+      which_term = LABEL;
+    }
+    if ((just_throwed_label < 5) && (which_term == LABEL)) {
+      which_term = BOTTLE;
+    }
+    if ((just_throwed_bottle < 5) && (which_term == BOTTLE)) {
+      //全員でよろこぶ！
+      //    thankYou(Motor_cap, Motor_label, Motor_bottle);
+
+      which_term = CAP;
+
+      //パタパタの「ぱ」状態に戻す
+      motorAngle_cap = LITTLE_OPEN;
+      motorAngle_label = LITTLE_OPEN;
+      motorAngle_bottle = LITTLE_OPEN;
+
+      n_cap = countNum;
+      n_label = countNum;
+      n_bottle = countNum;
+    }
+
+    //毎ループここでモーターの角度をmotorAngleにする
+    Motor_cap.write(motorAngle_cap);
+    Motor_label.write(motorAngle_label);
+    Motor_bottle.write(motorAngle_bottle);
+
+  }
+
+
+
+  if ((countNum % 50) == 0) {
+    Serial.print(motorAngle_cap);
+    Serial.print("::::");
+    Serial.print(motorAngle_label);
+    Serial.print("::::");
+    Serial.print(motorAngle_bottle);
+    Serial.print("::::");
+    Serial.print(n_cap);
+    Serial.print("::::");
+    Serial.println(countNum);
+  }
+
+  //  Serial.println(judge_infrared);
   countNum++;
   delay(10);
 }
 
-////距離センサに近づいたらモータがパカパカしだしてもっと近づいたら開く関数
-//void runMotor(int range, Servo motor) {
-//  if (range < 12){
-//    //12cmより近づいたら
-//    motor.write(120);
-//    piyo();
-//    delay(1000);
-//    motor.write(130);
-//    piyo();
-//  } else  if (range < 20 && range >= 12){
-//    //20cmより近づいたら
-//    delay(300);
-//    motor.write(175);
-//    delay(300);
-//    motor.write(150);
-//  } else {
-//    //ある程度離れてる段階
-//    motor.write(175);
-//  }
-//}
-//
-////ものが捨てられたら何かが起こる関数
-//// range:センサと物体の距離 motor:どのモータを動かすか
-//void trashThrowed(int range, Servo motor){
-//  if(range < 3){
-//    //センサから10cm以内のところに反応！→なにか捨てられたとき
-//    //喜んでパタパタってする
-//    for (int i = 0; i < 2; i++){
-//      delay(random(100, 200));
-//      motor.write(180);
-//      delay(random(100, 200));
-//      motor.write(160);
-//    }
-//  }
-//}
-//
 //void playTone(int tone, int duration) {
 //    for (long i = 0; i < duration * 1000L; i += tone * 2) {
 //        digitalWrite(speakerPin, HIGH);
